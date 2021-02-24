2f6cff35eab658cb9bd1a0eb7ce863e9d3f3b2e3
kai-fulfills <kai@fulfills.jp>
2021-02-24 18:28:41 +0900

    Move: Form Tag

--- admin/pages/includes/status-calender.php
+++ admin/pages/includes/status-calender.php
@@ -1,47 +1,49 @@
-<?php 
-    $day_label = [
-        __('Sunday'),
-        __('Monday'),
-        __('Tuesday'),
-        __('Wednesday'),
-        __('Thursday'),
-        __('Friday'),
-        __('Saturday'),
-    ];
-    $ymd_this = wp_date('Y-m');  // 今月の始め
-    $ymd_next = wp_date('Y-m', strtotime("$ymd_this + 1 month"));  // 来月の始め
-    $ymd_nextnext = wp_date('Y-m', strtotime("$ymd_this + 2 month"));  // 再来月の始め
-    foreach([
-        [strtotime(wp_date('Y-m-d')), strtotime("last day of $ymd_this")],
-        [strtotime($ymd_next), strtotime("last day of $ymd_next")],
-        [strtotime($ymd_nextnext), strtotime("last day of $ymd_nextnext")]
-    ] as $rerebf_time):
-    $y = intval(wp_date('Y', $rerebf_time[0])); // 年（変数）
-    $m = intval(wp_date('m', $rerebf_time[0])); // 月（変数）
-    $d_f = intval(wp_date('d', $rerebf_time[0]));
-    $d_l = intval(wp_date('d', $rerebf_time[1]));   // 最終日（日）
-?>
-<h3><?php echo wp_date(get_option('date_format'), $rerebf_time[0]);?> - <?php echo wp_date(get_option('date_format'), $rerebf_time[1]);?></h3>
-<div class="table">
-    <?php foreach($day_label as $label):?>
-    <div class="label"><?php echo $label;?></div>
+<form class="calendar rerebf-store" rerebf="status">
+    <?php 
+        $day_label = [
+            __('Sunday'),
+            __('Monday'),
+            __('Tuesday'),
+            __('Wednesday'),
+            __('Thursday'),
+            __('Friday'),
+            __('Saturday'),
+        ];
+        $ymd_this = wp_date('Y-m');  // 今月の始め
+        $ymd_next = wp_date('Y-m', strtotime("$ymd_this + 1 month"));  // 来月の始め
+        $ymd_nextnext = wp_date('Y-m', strtotime("$ymd_this + 2 month"));  // 再来月の始め
+        foreach([
+            [strtotime(wp_date('Y-m-d')), strtotime("last day of $ymd_this")],
+            [strtotime($ymd_next), strtotime("last day of $ymd_next")],
+            [strtotime($ymd_nextnext), strtotime("last day of $ymd_nextnext")]
+        ] as $rerebf_time):
+        $y = intval(wp_date('Y', $rerebf_time[0])); // 年（変数）
+        $m = intval(wp_date('m', $rerebf_time[0])); // 月（変数）
+        $d_f = intval(wp_date('d', $rerebf_time[0]));   // 開始日
+        $d_l = intval(wp_date('d', $rerebf_time[1]));   // 最終日
+    ?>
+    <h3><?php echo wp_date(get_option('date_format'), $rerebf_time[0]);?> - <?php echo wp_date(get_option('date_format'), $rerebf_time[1]);?></h3>
+    <div class="table">
+        <?php foreach($day_label as $label):?>
+        <div class="label"><?php echo $label;?></div>
+        <?php endforeach;?>
+        <?php for($i = 0; $i < intval(wp_date('w', $rerebf_time[0])); $i++) echo '<div class="blank"></div>'; // 開始曜日の調整 ?>
+        <?php for($d = $d_f; $d <= $d_l; $d++):?>
+            <?php $day = new rerebfDay($y, $m, $d);?>
+            <div class="active <?php echo ((wp_date('Ym', $rerebf_time[0]) === wp_date('Ym') && $d == intval(wp_date('d'))) ? 'today' : '');?>">
+                <div class="date"><?php echo $d;?></div>
+                <label class="is_open">
+                    <?php if(!$day->data['is_open']):?>
+                        <input type="checkbox" name="<?php echo sprintf('%04d', $y).sprintf('%02d', $m).sprintf('%02d', $d);?>" value="1">
+                    <?php endif;?>
+                    <span class="open">OPEN</span>
+                    <span class="close">CLOSED</span>
+                </label>
+                <?php // var_dump($day);?>
+            </div>
+        <?php endfor;?>
+        <?php for($i = intval(wp_date('w', $rerebf_time[1])) + 1; $i < 7; $i++) echo '<div class="blank"></div>'; // 終了曜日の調整 ?>
+    </div>
     <?php endforeach;?>
-    <?php for($i = 0; $i < intval(wp_date('w', $rerebf_time[0])); $i++) echo '<div class="blank"></div>'; // 開始曜日の調整 ?>
-    <?php for($d = $d_f; $d <= $d_l; $d++):?>
-        <?php $day = new rerebfDay($y, $m, $d);?>
-        <div class="active <?php echo ((wp_date('Ym', $rerebf_time[0]) === wp_date('Ym') && $d == intval(wp_date('d'))) ? 'today' : '');?>">
-            <div class="date"><?php echo $d;?></div>
-            <label class="is_open">
-                <?php if(!$day->data['is_open']):?>
-                    <input type="checkbox" name="<?php echo sprintf('%04d', $y).sprintf('%02d', $m).sprintf('%02d', $d);?>" value="1">
-                <?php endif;?>
-                <span class="open">OPEN</span>
-                <span class="close">CLOSED</span>
-            </label>
-            <?php // var_dump($day);?>
-        </div>
-    <?php endfor;?>
-    <?php for($i = intval(wp_date('w', $rerebf_time[1])) + 1; $i < 7; $i++) echo '<div class="blank"></div>'; // 終了曜日の調整 ?>
-</div>
-<?php endforeach;?>
-<input type="submit" class="button button-primary rerebf-store" value="変更を保存する">
+    <input type="submit" class="button button-primary rerebf-store" value="変更を保存する">
+</form>
--- admin/pages/status.php
+++ admin/pages/status.php
@@ -4,12 +4,10 @@
     // トップページ
     function rerebf_addpage_status() {
 ?>
-        <form class="wrap rerebf-store" id="rerebf-status" rerebf="status">
-            <h1><?php _e('Status', 'rerebf');?> - Restaurant Reservations By FULFILLs</h1>
-            <h2>予約カレンダー</h2>
-            <div class="calendar">
-                <?php include 'includes/status-calender.php';?>
-            </div>
-        </form>
+    <div class="wrap" id="rerebf-status">
+        <h1><?php _e('Status', 'rerebf');?> - Restaurant Reservations By FULFILLs</h1>
+        <h2>予約カレンダー</h2>
+        <?php include 'includes/status-calender.php';?>
+    </div>
 <?php
 }
